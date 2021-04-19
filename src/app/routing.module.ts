1dc4def415f829ffb27db66ef52ff4bc66441ec2
sarthak143 <sarthak.tiet@gmail.com>
2021-04-20 00:01:42 +0530

    1 file modified

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+// import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { HeaderComponent } from "./includes/header/header.component";
+export const routes: Routes = [
+  // { path: "login", component: LoginComponent },
+  // {
+  //   path: "",
+  //   pathMatch: "full",
+  //   redirectTo: "/login"
+  // },
+  // { path: "**", component: NotfoundComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class RoutingModule { }
+export class RoutingModule {}
