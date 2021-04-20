782b37be75fd7cff2258bc692629a2bf8c312d34
sarthak143 <sarthak.tiet@gmail.com>
2021-04-20 19:38:53 +0530

    1 file modified

--- src/app/observables/fromevent/fromevent.component.ts
+++ src/app/observables/fromevent/fromevent.component.ts
@@ -1,22 +1,19 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { fromEvent } from "rxjs";
 
 @Component({
-  selector: 'app-from-event',
-  templateUrl: './from-event.component.html',
-  styleUrls: ['./from-event.component.css']
+  selector: "app-from-event",
+  templateUrl: "./from-event.component.html",
+  styleUrls: ["./from-event.component.css"]
 })
 export class FromEventComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  @ViewChild() addBtn: ElementRef;
+  @ViewChild("addBtn") addBtn: ElementRef;
 
   ngOnInit() {
-        fromEvent(this.addBtn.nativeElement, 'click').subscribe(res =>{
-
-          console.log(res);
-        })
+    fromEvent(this.addBtn.nativeElement, "click").subscribe(res => {
+      console.log(res);
+    });
   }
-
 }
