688c9eca4ab6a3a9cb60408d1f0ba09574a97414
sarthak143 <sarthak.tiet@gmail.com>
2021-04-20 11:27:38 +0530

    2 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { HeaderComponent } from "./includes/header/header.component";
 import { RoutingModule } from "./routing.module";
-import { PromiseComponent } from './promise/promise.component';
+import { PromiseComponent } from "./promise/promise.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, RoutingModule],
-  declarations: [AppComponent, HelloComponent, HeaderComponent, PromiseComponent],
+  imports: [BrowserModule, FormsModule, RoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeaderComponent,
+    PromiseComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+class Post {
+  constructor(
+    public userId: number,
+    public id: string,
+    public title: string,
+    public body: string
+  ) {}
+}
 
 @Component({
-  selector: 'app-promise',
-  templateUrl: './promise.component.html',
-  styleUrls: ['./promise.component.css']
+  selector: "app-promise",
+  templateUrl: "./promise.component.html"
+  // styleUrls: ['./promise.component.css']
 })
 export class PromiseComponent implements OnInit {
+  api: string = "https://jsonplaceholder.typicode.com/posts";
+  data = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.getPosts();
   }
 
+  getPosts() {
+    const promise = new Promise((resolve, reject) => {
+      const apiURL = this.api;
+      this.http
+        .get<Post[]>(apiURL)
+        .toPromise()
+        .then(
+          (res: any) => {
+            // Success
+            this.data = res.map((res: any) => {
+              return new Post(res.userId, res.id, res.title, res.body);
+            });
+            resolve();
+          },
+          err => {
+            // Error
+            reject(err);
+          }
+        );
+    });
+    return promise;
+  }
 }
