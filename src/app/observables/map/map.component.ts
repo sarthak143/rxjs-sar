2c0d55985b8ad742a87eca5a1db84152b9974029
sarthak143 <sarthak.tiet@gmail.com>
2021-04-21 22:45:13 +0530

    1 file modified

--- src/app/observables/map/map.component.ts
+++ src/app/observables/map/map.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { interval, Subscription } from "rxjs";
+import { take, toArray } from "rxjs/operators";
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  selector: "app-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.css"]
 })
 export class MapComponent implements OnInit {
-
-  constructor() { }
+Subs: Subscription;
+  constructor() {}
 
   ngOnInit() {
+    const source = interval(200);
+    this.Subs = source
+      .pipe(
+        take(5),
+        toArray()
+      )
+      .subscribe(res => {
+        console.log(res);
+        this.addel.addList(res, "appendlist");
+      });
   }
-
 }
