546fb5e831b1f142456dc1eafc8d5cfa273e6614
sarthak143 <sarthak.tiet@gmail.com>
2021-04-21 20:21:49 +0530

    1 file modified, 1 added

--- src/app/observables/to-array/to-array.component.ts
+++ src/app/observables/to-array/to-array.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { interval } from "rxjs/internal/observable/interval";
 
 @Component({
-  selector: 'app-to-array',
-  templateUrl: './to-array.component.html',
-  styleUrls: ['./to-array.component.css']
+  selector: "app-to-array",
+  templateUrl: "./to-array.component.html",
+  styleUrls: ["./to-array.component.css"]
 })
 export class ToArrayComponent implements OnInit {
-
-  constructor() { }
+  let Subs:Subscription;
+  obsMsg;
+  constructor() {}
 
   ngOnInit() {
+    const broadcastVideo = interval(1000);
+    this.Subs = broadcastVideo.subscribe(res => {
+      console.log(res);
+      this.obsMsg = "Video " + res;
+      this.addel.addList(this.obsMsg, "appendlist");
+      if (res >= 5) {
+        this.Subs.unsubscribe();
+      }
+    });
   }
-
 }
+
