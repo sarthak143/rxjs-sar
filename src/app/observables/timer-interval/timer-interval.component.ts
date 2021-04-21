be4d4fdb2cd1e2ffbd529a07c4eb0b3abaea8758
sarthak143 <sarthak.tiet@gmail.com>
2021-04-21 14:56:55 +0530

    1 file modified

--- src/app/observables/timer-interval/timer-interval.component.ts
+++ src/app/observables/timer-interval/timer-interval.component.ts
@@ -16,7 +16,7 @@ export class TimerIntervalComponent implements OnInit {
     const broadcastVideo = interval(1000);
     this.videoSubs = broadcastVideo.subscribe(res => {
       console.log(res);
-      this.obsMsg = "Video" + res;
+      this.obsMsg = "Video " + res;
       this.addel.addList(this.obsMsg);
       if (res >= 5) {
         this.videoSubs.unsubscribe();
