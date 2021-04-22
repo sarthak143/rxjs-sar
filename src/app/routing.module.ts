9be90fc97b88f28d37cbcfec72450c522eb99103
sarthak143 <sarthak.tiet@gmail.com>
2021-04-22 20:00:32 +0530

    1 file modified

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -12,6 +12,17 @@ import { OfFromComponent } from "./observables/of-from/of-from.component";
 import { ToArrayComponent } from "./observables/to-array/to-array.component";
 import { MapComponent } from "./observables/map/map.component";
 import { FilterComponent } from "./observables/filter/filter.component";
+import { MergeMapComponent } from "./observables/merge-map/merge-map.component";
+import { ConcatMapComponent } from "./observables/concat-map/concat-map.component";
+import { MergeComponent } from "./observables/merge/merge.component";
+import { CatchTryErrorHandlingComponent } from "./observables/catch-try-error-handling/catch-try-error-handling.component";
+import { DebounceTimeDistinctUnitChangedComponent } from "./observables/debounce-time-distinct-unit-changed/debounce-time-distinct-unit-changed.component";
+import { ReplaySubjectComponent } from "./observables/replay-subject/replay-subject.component";
+import { AsyncSubjectComponent } from "./observables/async-subject/async-subject.component";
+import { ReduceComponent } from "./observables/reduce/reduce.component";
+import { TapComponent } from "./observables/tap/tap.component";
+import { FlatMapComponent } from "./observables/flat-map/flat-map.component";
+import { PluckComponent } from "./observables/pluck/pluck.component";
 
 export const routes: Routes = [
   {
@@ -29,6 +40,14 @@ export const routes: Routes = [
       { path: "timerInterval", component: TimerIntervalComponent },
       { path: "OfFrom", component: OfFromComponent },
       { path: "map", component: MapComponent },
+      { path: "fliter", component: FilterComponent },
+      { path: "MergeMapComponent", component: MergeMapComponent },
+      { path: "fliter", component: FilterComponent },
+      { path: "fliter", component: FilterComponent },
+      { path: "fliter", component: FilterComponent },
+      { path: "fliter", component: FilterComponent },
+      { path: "fliter", component: FilterComponent },
+      { path: "fliter", component: FilterComponent },
       { path: "fliter", component: FilterComponent },
       { path: "ToArray", component: ToArrayComponent }
     ]
